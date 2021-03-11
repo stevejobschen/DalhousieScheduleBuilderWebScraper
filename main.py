4db644c1f79b952c94b79fc735d091934e5bf54e
SteveChen <steve.chen@dal.ca>
2021-03-11 15:20:27 -0400

    Code Clean up

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from bs4 import BeautifulSoup
 import requests
 import db
+from settings import *
+
 
 class Course:
     def __init__(self, title, classes):
@@ -15,13 +17,15 @@ class Course:
                 self.title += " " + w
 
         self.classes = classes
-        
+
+
 def isHeaderRow(row):
     try:
         row['valign']
         return True
     except:
         return False
+
 
 def parseDate(times):
     days = []
@@ -32,8 +36,9 @@ def parseDate(times):
             days.append(parseDoubleDate(time))
         else:
             days.append(1)
-            #print(days)
+            # print(days)
     return days
+
 
 def parseDoubleDate(time):
     prevTime = time.p.br.previous_sibling
@@ -47,23 +52,24 @@ def parseDoubleDate(time):
     else:
         return 0
 
+
 def parseCourse(course_data):
-    termNub=course_data[1].find_all('td')[1].find('b').string+"" #get crn and get first letter
+    termNub = course_data[1].find_all('td')[1].find('b').string + ""  # get crn and get first letter
     # print(termNub[0])
-    
-    title = course_data[0].find('b').string+" "+termNub[0] # parse title into CSCI CODE and Course Name +Add ter,number to the end of the object
+
+    title = course_data[0].find('b').string + " " + termNub[
+        0]  # parse title into CSCI CODE and Course Name +Add ter,number to the end of the object
     # print(title)
     courses = []
 
     for i in range(1, len(course_data)):
         courseInfo = course_data[i].find_all('td')
 
-
         try:
-            #if there is notation on location
+            # if there is notation on location
             if (courseInfo[12].string == None):
-                loc_one = courseInfo[12].br.previous_sibling.string +""
-                if  loc_one == "*** 07-JAN-2019 - 05-APR-2019 ***":
+                loc_one = courseInfo[12].br.previous_sibling.string + ""
+                if loc_one == "*** 07-JAN-2019 - 05-APR-2019 ***":
 
                     course = {}
                     course['crn'] = courseInfo[1].find('b').string
@@ -94,7 +100,8 @@ def parseCourse(course_data):
                     if (course['current'] == None):
                         open_curr = courseInfo[14].p.br.previous_sibling
                         disp_curr = courseInfo[14].p.br.next_sibling
-                        course['current'] = "FIRST("+ open_curr.replace(" ", "") + ") SEC("+ disp_curr.replace(" ", "") + ")"
+                        course['current'] = "FIRST(" + open_curr.replace(" ", "") + ") SEC(" + disp_curr.replace(" ",
+                                                                                                                 "") + ")"
 
                     try:
                         course['waitlist'] = courseInfo[16].p.string
@@ -108,7 +115,8 @@ def parseCourse(course_data):
                     else:
                         first_prof = courseInfo[20].br.previous_sibling
                         sec_prof = courseInfo[20].br.next_sibling
-                        course['prof'] = "ONE("+first_prof.strip(' \t\n\r') + ") TWO(" + sec_prof.strip(' \t\n\r') + ")"
+                        course['prof'] = "ONE(" + first_prof.strip(' \t\n\r') + ") TWO(" + sec_prof.strip(
+                            ' \t\n\r') + ")"
 
             # Normal situation
             else:
@@ -141,7 +149,8 @@ def parseCourse(course_data):
                 if (course['current'] == None):
                     open_curr = courseInfo[14].p.br.previous_sibling
                     disp_curr = courseInfo[14].p.br.next_sibling
-                    course['current'] = "FIRST("+ open_curr.replace(" ", "") + ") SEC("+ disp_curr.replace(" ", "") + ")"
+                    course['current'] = "FIRST(" + open_curr.replace(" ", "") + ") SEC(" + disp_curr.replace(" ",
+                                                                                                             "") + ")"
 
                 try:
                     course['waitlist'] = courseInfo[16].p.string
@@ -155,7 +164,7 @@ def parseCourse(course_data):
                 else:
                     first_prof = courseInfo[20].br.previous_sibling
                     sec_prof = courseInfo[20].br.next_sibling
-                    course['prof'] = "ONE("+first_prof.strip(' \t\n\r') + ") TWO(" + sec_prof.strip(' \t\n\r') + ")"
+                    course['prof'] = "ONE(" + first_prof.strip(' \t\n\r') + ") TWO(" + sec_prof.strip(' \t\n\r') + ")"
 
             courses.append(course)
         except Exception as e:
@@ -164,12 +173,13 @@ def parseCourse(course_data):
     c = Course(title, courses)
     return c
 
+
 def parseUrl(url):
     pageCount = 1
     courses = []
     j = 0
     while j < pageCount:
-        newUrl = url + "&s_numb=&n=" + setPageHelper(j+1)
+        newUrl = url + "&s_numb=&n=" + setPageHelper(j + 1)
         html_doc = requests.get(newUrl).text
         print(newUrl)
         # use bs4
@@ -183,30 +193,31 @@ def parseUrl(url):
             pageCount = 1
 
         # MARK: ROWS
-        #for w in soup.find_all('select')[1].find_all('option')[1:]:
-         #    print("\""+w['value'], end='\",')
-        
-        #read table line by line
+        # for w in soup.find_all('select')[1].find_all('option')[1:]:
+        #    print("\""+w['value'], end='\",')
+
+        # read table line by line
         rows = soup.find_all('table', attrs={'class': 'dataentrytable'})[1].find_all('tr')
 
         headerIndexs = []
-        for i , row in enumerate(rows):
+        for i, row in enumerate(rows):
             if isHeaderRow(rows[i]):
                 headerIndexs.append(i)
-       
+
         courses_raw = []
         for i in range(0, len(headerIndexs) - 1):
-            course_raw = rows[headerIndexs[i]:(headerIndexs[i+1])]
+            course_raw = rows[headerIndexs[i]:(headerIndexs[i + 1])]
             courses_raw.append(course_raw)
-        #print(course_raw)
+        # print(course_raw)
         course_raw = rows[headerIndexs[len(headerIndexs) - 1]:]
         courses_raw.append(course_raw)
 
         for course in courses_raw:
-            #print(parseCourse(course))
+            # print(parseCourse(course))
             courses.append(parseCourse(course))
         j += 1
     return courses
+
 
 def setPageHelper(page):
     if page < 1:
@@ -216,46 +227,17 @@ def setPageHelper(page):
     else:
         return str((20 * (page - 1)) + 1)
 
-def main():
-    # fall subjects
-    #subjects = ["ACSC", "ANAT", "ARTC", "ARBC", "ARCH", "ASSC", "BIOC", "BIOE", "BIOL", "BMNG", "BUSI", "CANA", "CHEE", "CHEM", "CHIN", "CIVL", "CLAS", "COMM", "CH_E", "CPST", "CSCI", "CTMP", "CRWR", "DEHY", "DENT", "DMUT", "EMSP", "ERTH", "ECON", "ECED", "ECMM", "ENGI", "INWK", "ENGM", "ENGL", "ENSL", "ENVE", "ENVS", "ENVI", "EURO", "FILM", "FIGA", "FIGS", "FOSC", "FREN", "GWST", "GEOG", "GERM", "HESA", "HINF", "HLTH", "HPRO", "HSCE", "HAHP", "HSTC", "HIST", "HUCD", "INDG", "IENG", "INFX", "INFO", "INTE", "INTD", "IPHE", "ITAL", "JOUR", "KINE", "KING", "LAWS", "LEIS", "MRIT", "MGMT", "MARA", "MARI", "MATL", "MATH", "MTHA", "MECH", "MEDP", "MEDR", "MICI", "MINE", "MUSC", "NESC", "NUMT", "NURS", "OCCU", "OCEA", "ORAL", "PHDP", "PATH", "PERF", "PERI", "PHAC", "PHAR", "PHIL", "PHYC", "PHYL", "PHYT", "PLAN", "POLI", "PGPH", "PEAS", "PROS", "PSYR", "PSYO", "PUAD", "RADT", "REGN", "RELS", "RSPT", "RUSN", "SCIE", "SLWK", "SOSA", "SPAN", "STAT", "SUST", "THEA", "TYPR", "VISC"]
-    # winter subjects
-    #subjects = ["ACSC","ANAT","ARBC","ARCH","ASSC","BIOC","BIOE","BIOL","BMNG","BUSI","CANA","CNLT","CHEE","CHEM","CHIN","CIVL","CLAS","COMM","CH_E","CPST","CSCI","CTMP","CRWR","DEHY","DENT","DMUT","EMSP","ERTH","ECON","ECED","ECMM","ENGI","INWK","ENGM","ENGL","ENSL","ENVE","ENVS","ENVI","EURO","FILM","FOSC","FREN","GWST","GEOG","GERM","HESA","HINF","HLTH","HPRO","HSCE","HAHP","HSTC","HIST","HUCD","INDG","IENG","INFO","INTE","INTD","IPHE","ITAL","JOUR","KINE","KING","LAWS","LEIS","MRIT","MGMT","MARA","MARI","MATL","MATH","MECH","MDLT","MEDP","MEDR","MICI","MINE","MUSC","NESC","NUMT","NURS","OCCU","OCEA","ORAL","PHDP","PATH","PERF","PERI","PHAC","PHAR","PHIL","PHYC","PHYL","PHYT","PLAN","POLI","PGPH","PEAS","PROS","PSYR","PSYO","PUAD","RADT","REGN","RELS","RSPT","RUSN","SCIE","SLWK","SOSA","SPAN","STAT","SUST","THEA","TYPR","VISC"]
-    # 2019 summer subjects
-    subjects2020SUMMER = ["ANAT", "ASSC", "BIOC", "BIOT", "BIOE", "BIOL", "BMNG", "BUSI", "CANA", "CHEE", "CHEM", "CIVL", "CLAS", "COMM","CMSD", "CH_E", "CPST", "CSCI", "DEHY", "DENT", "DMUT", "EMSP", "ERTH", "ECON", "ECED", "ECMM", "ENGI", "INWK", "ENGM", "ENGL", "ENSL", "ENVE", "ENVS", "ENVI", "FOSC", "FREN", "GWST", "GEOG", "GERM", "HESA", "HINF", "HLTH", "HPRO", "HSTC", "HIST", "IENG", "INFO", "INTE", "INTD", "IPHE", "JOUR", "KINE", "LAWS", "LEIS", "MGMT", "MARA", "MARI", "MATL", "MATH", "MECH", "MEDP", "MEDR", "MEDI", "MICI", "MINE", "MUSC", "NESC", "NUMT", "NURS", "OCCU", "OCEA", "ORAL", "PHDP", "PATH", "PERI", "PHAC", "PETR", "PHAR", "PHAC" , "PHIL", "PHYC", "PHYL", "PHYT", "PLAN", "POLI", "PGPH", "PEAS", "PSYR", "PSYO", "PUAD", "RADT", "REGN", "RELS", "RSPT", "RUSN", "SCIE", "SLWK", "SOSA", "SPAN", "STAT", "SUST", "THEA", "VISC"]
-    #2020 Fall
-    subjects2020FALL = ["ACSC", "ANAT","ARBC", "ARCH", "ASSC", "BIOC", "BIOE", "BIOL", "BMNG", "BUSI", "CANA", "CHEE", "CHEM", "CHIN", "CIVL", "CLAS", "COMM", "CH_E", "CPST", "CSCI", "CTMP", "CRWR", "DEHY", "DENT", "DMUT", "EMSP", "ERTH", "ECON", "ECED", "ECMM", "ENGI", "ENGM", "ENGL", "ENSL", "ENVE", "ENVS", "ENVI", "EURO", "FILM", "FIGA", "FIGS", "FOSC", "FREN", "GWST", "GEOG", "GERM", "HESA", "HINF", "HLTH", "HPRO", "HSCE", "HAHP", "HSTC", "HIST", "INDG", "IENG", "INFO", "INTE", "INTD", "IPHE", "ITAL", "JOUR", "KINE", "KING", "LAWS", "LEIS", "MRIT", "MGMT", "MARA", "MARI", "MATL", "MATH", "MECH", "MEDP", "MEDR", "MICI", "MINE", "MUSC", "NESC", "NUMT", "NURS", "OCCU", "OCEA", "ORAL", "PHDP", "PATH", "PERF", "PERI", "PHAC", "PHAR", "PHIL", "PHYC", "PHYL", "PHYT", "PLAN", "POLI", "PGPH", "PEAS", "PROS", "PSYR", "PSYO", "PUAD", "RADT", "REGN", "RELS", "RSPT", "RUSN", "SCIE", "SLWK", "SOSA", "SPAN", "STAT", "SUST", "THEA", "TYPR", "VISC"]
-    # test subjects
-    #subjects =  ["CSCI","INFO", "INTE", "INTD", "IPHE", "ITAL", "JOUR", "KINE", "KING", "LAWS", "LEIS", "MRIT", "MGMT", "MARA", "MARI", "MATL", "MATH", "MECH", "MEDP", "MEDR", "MICI", "MINE", "MUSC", "NESC", "NUMT", "NURS", "OCCU", "OCEA", "ORAL", "PHDP", "PATH", "PERF", "PERI", "PHAC", "PHAR", "PHIL", "PHYC", "PHYL", "PHYT", "PLAN", "POLI", "PGPH", "PEAS", "PROS", "PSYR", "PSYO", "PUAD", "RADT", "REGN", "RELS", "RSPT", "RUSN", "SCIE", "SLWK", "SOSA", "SPAN", "STAT", "SUST", "THEA", "TYPR", "VISC"]
 
-    
-    terms = {
-    "2018winter":"201920",
-    "2018fall":"201910",
-    "2018fall/winter":"201910%\2C201920",
-    "2019Summer":"201930",
-    "2019fall/winter":"202010,202020"
-    }
-    district = "100"
-    
+def main():
     database = db.Database()
     # Fall and winter cource
-    for subject in subjects2020FALL:
-        url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term="+ terms["2019fall/winter"] + "&s_subj="+ subject + "&s_district=" + district
+    for subject in Settings.semester:
+        url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term=" + Settings.termsLibrary[Settings.terms] + "&s_subj=" + subject + "&s_district=" + Settings.district
 
-                # THIS IS THE FINAL ARRAY WITH ALL INFORMATION IN IT
-                # use as data[courseindex] .title or .classes[classindex] for full info
-        data = parseUrl(url)
-        database.saveCourses(data)
-
-    # Summer cource
-    for subject in subjects2020SUMMER:
-        url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term="+ terms["2019Summer"] + "&s_subj="+ subject + "&s_district=" + district
-
-                # THIS IS THE FINAL ARRAY WITH ALL INFORMATION IN IT
-                # use as data[courseindex] .title or .classes[classindex] for full info
+        # THIS IS THE FINAL ARRAY WITH ALL INFORMATION IN IT
+        # use as data[courseindex] .title or .classes[classindex] for full info
         data = parseUrl(url)
         database.saveCourses(data)
 
 if __name__ == "__main__":
-  main()
+    main()
